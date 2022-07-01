3c0660b30bb671446d912ae37676e770cc535656
efbolton <EFBOLTON@HOTMAIL.COM>
2022-07-01 08:48:40 -0400

    20220701

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "animate.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img
+          src={logo}
+          className="App-logo animate__animated animate__infinite animate__rubberBand"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link "
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
